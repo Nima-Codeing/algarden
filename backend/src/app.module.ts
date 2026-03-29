2764b429fad543ac483e0dd3f759da4880589735
B-Beens <161596540+Nima-Codeing@users.noreply.github.com>
2026-03-30 06:37:07 +0900

    chore(prisma): define initial prisma client

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, TodoModule],
   controllers: [],
   providers: [],
 })
