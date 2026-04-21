01590fba1c5703dcace94146cc914104a5fd8ab2
B-Beens <161596540+Nima-Codeing@users.noreply.github.com>
2026-04-22 06:25:32 +0900

    feat(be/auth): /auth endpoint initialize

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule],
+  imports: [PrismaModule, TodoModule, AuthModule],
   controllers: [],
   providers: [],
 })
