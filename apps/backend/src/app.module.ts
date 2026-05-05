ec67e342c4553e640ded5ddc2962e340927788aa
B-Beens <161596540+Nima-Codeing@users.noreply.github.com>
2026-05-05 14:00:56 +0900

    feat(be/garden): /garden endpoint initialize

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { GardenModule } from './garden/garden.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule, AuthModule],
+  imports: [PrismaModule, TodoModule, AuthModule, GardenModule],
   controllers: [],
   providers: [],
 })
