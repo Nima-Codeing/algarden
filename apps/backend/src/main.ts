dae0e2bfd4eb66016515dd7d1f66113a090f8345
B-Beens <161596540+Nima-Codeing@users.noreply.github.com>
2026-05-08 06:23:14 +0900

    fix(be): add credentials to CORS config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
