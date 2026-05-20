ad7835efc6e91c43b80df91686adfe92f6ca9012
B-Beens <161596540+Nima-Codeing@users.noreply.github.com>
2026-05-21 06:20:28 +0900

    chore(be/seed): scaffold SeedModule, Service, Controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { TodoModule } from './todo/todo.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { GardenModule } from './garden/garden.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule, AuthModule, GardenModule],
+  imports: [PrismaModule, TodoModule, AuthModule, GardenModule, SeedModule],
   controllers: [],
   providers: [],
 })
