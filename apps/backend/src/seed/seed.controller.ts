bc28e02734a46740ad39a0bcd2f516b9e8dab3c2
Nima <161596540+Nima-Codeing@users.noreply.github.com>
2026-05-22 07:17:22 +0900

    feat(be/seed): add JWT guard to SeedController

--- apps/backend/src/seed/seed.controller.ts
+++ apps/backend/src/seed/seed.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { Seed } from 'generated/prisma/client';
 import { Request } from 'express';
 import { RequestUser } from 'src/auth/types/requsetUser.types';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('seeds')
+@UseGuards(AuthGuard('jwt'))
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
