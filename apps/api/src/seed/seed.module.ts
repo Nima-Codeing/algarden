5f902eb5701c9d1053e3d9ece36da27603b1c5d0
Nima <161596540+Nima-Codeing@users.noreply.github.com>
2026-05-27 08:39:38 +0900

    refactor(api/seed): use direct Prisma query to avoid circular dependency

--- apps/api/src/seed/seed.module.ts
+++ apps/api/src/seed/seed.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { GardenModule } from 'src/garden/garden.module';
 
 @Module({
-  imports: [GardenModule],
   controllers: [SeedController],
   providers: [SeedService],
 })
