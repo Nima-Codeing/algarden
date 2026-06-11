4cadd0b2562a2b08cf8227f53465492c97a92948
Nima <161596540+Nima-Codeing@users.noreply.github.com>
2026-06-11 22:48:43 +0900

    feat(web/garden): scaffold GardenCanvas component

---
+++ apps/web/src/feature/garden/components/GardenCanvas.tsx
@@ -0,0 +1,10 @@
+export const GardenCanvas = () => {
+  return (
+    <svg
+      className="border-4 border-purple-400"
+      width="200"
+      height="200"
+      viewBox="0 0 100 100"
+    ></svg>
+  );
+};
