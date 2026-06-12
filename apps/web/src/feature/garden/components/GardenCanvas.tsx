ddd1dbc7748bdecf5b622df72a459c703578eaba
Nima <161596540+Nima-Codeing@users.noreply.github.com>
2026-06-12 23:19:37 +0900

    feat(web/garden): display node using mock data

--- apps/web/src/feature/garden/components/GardenCanvas.tsx
+++ apps/web/src/feature/garden/components/GardenCanvas.tsx
@@ -1,10 +1,25 @@
+import { mockPlant } from "../mockNodeData";
+
 export const GardenCanvas = () => {
+  const SATURATION = 80;
+  const BRIGHTNESS = 65;
+  const data = mockPlant;
   return (
     <svg
-      className="border-4 border-purple-400"
+      className="border-4 border-purple-400 bg-mist-900"
       width="200"
       height="200"
-      viewBox="0 0 100 100"
-    ></svg>
+      viewBox="0 0 400 400"
+    >
+      {data.plantNodes.map((n) => (
+        <circle
+          key={n.id}
+          cx={100}
+          cy={200}
+          r={n.size / 2}
+          fill={`hsl(${n.hue} ${SATURATION}% ${BRIGHTNESS}%)`}
+        />
+      ))}
+    </svg>
   );
 };
