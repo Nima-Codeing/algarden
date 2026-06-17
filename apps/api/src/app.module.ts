4ffaf3dd8a527e9446557fbe5fba9ea2f0704bed
Nima <161596540+Nima-Codeing@users.noreply.github.com>
2026-06-18 05:38:15 +0900

    feat(api/plant): add Plant model

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { GardenModule } from './garden/garden.module';
 import { SeedModule } from './seed/seed.module';
+import { PlantModule } from './plant/plant.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule, AuthModule, GardenModule, SeedModule],
+  imports: [PrismaModule, TodoModule, AuthModule, GardenModule, SeedModule, PlantModule],
   controllers: [],
   providers: [],
 })
